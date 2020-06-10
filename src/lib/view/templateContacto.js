ef0aaeec0b274a7b7533c24afc0e1a0b6ea87934
Francisca <franciscabelen112002@gmail.com>
2020-06-10 17:36:31 -0400

    template contacto

--- src/lib/view/templateContacto.js
+++ src/lib/view/templateContacto.js
@@ -3,6 +3,13 @@ export const contacto = () => {
   const viewContacto = `   
   <div id="banner"></div>
   
+<div class="container-contacto">
+
+        <div class="contactTittle">
+            <h2 class="tituloContact">Contáctanos</h2>
+            <p>Envíanos tu duda o sugerencia</p>
+        </div>
+
   <div class="info-ayudamos">
     <p>
         En SocialBeer queremos ayudarte, si necesitas contactarte con nosotros lo puedes hacer llamando al 
@@ -15,69 +22,51 @@ export const contacto = () => {
 <div class="formAyuda">
 
         <form id="contactform">
-            <div class="contactTittle">
-                <h2 class="tituloContact">Contáctanos</h2>
-                <p>Envíanos tu duda o sugerencia</p>
-            </div>
 
             <div id="contactname">
-                <label for="name">Nombre Completo:<span class="textForm">*</span></label>
+                <label for="name">Nombre Completo:<span class="textForm"></span></label>
                 <input type="text" name="name" id="nombre" class="" placeholder="Nombre Completo">
             </div>
 
             <div id="contactmail">
-                <label for="email">Dirección de Correo Electrónico:<span class="textForm">*</span></label>
+                <label for="email">Dirección de Correo Electrónico:<span class="textForm"></span></label>
                 <input type="text" name="email" id="email" class="" placeholder="ej: mail@mail.com">
             </div>
 
             <div id="contactphone">
-                <label for="phone">Teléfono:<span class="textForm">*</span></label>
+                <label for="phone">Teléfono:<span class="textForm"></span></label>
                 <input type="tel" name="phone" id="phone" class="" placeholder=" 56 9 222 22 222">
             </div>
 
 
             <div id="contactcity">
-                <label for="ciudad">Ciudad<span class="textForm">*</span>:</label>
+                <label for="ciudad">Ciudad<span class="textForm"></span>:</label>
                 <input type="text" name="ciudad" id="ciudad" class="" placeholder="Ej: Santiago" required>
             </div>
 
             <div id="contactsubject">
-                <label for="asunto">Asunto<span class="textForm">*</span>:</label>
+                <label for="asunto">Asunto<span class="textForm"></span>:</label>
                 <input type="text" name="asunto" id="asunto" class="" placeholder="Ingrese su asunto" required>
             </div>
 
             <div id="contactAsk">
 
-                <div coment">
+                <div id ="coment">
 
-                    <label for="coment">Consulta:<span class="textForm">*</span></label>
+                    <label for="coment">Consulta:<span class="textForm"></span></label>
                     <textarea name="coment" id="coment" cols="50" rows="5"></textarea>
 
                 </div>
 
-                <div class="textoComentario">
-
-                    <p>Máximo 700 caracteres / <span class="textForm">*</span>Campos Obligatorio </p>
-
-                </div>
-
             </div>
 
-            <div id="contactbtn"">
+            <div id="contactbtn">
 
-                <input type="submit" id="submit" class="btnSend" name="submit" value="Enviar">
+                <input type="submit" id="submit" class="btnSend btnInicio" name="submit" value="Enviar">
 
             </div>
 
         </form>
-
-    </div>
-
-    <div id="contactok">
-
-        <h2 id="AprobadoTitle">Gracias por escribirnos</h2>
-        <p class="lead">Sus datos han sido enviados.</p>
-        <a class="close-modal" aria-label="Close">&#215;</a>
 
     </div>
 `

fda0282b50ed0cd7b42591054c78bb7347adcde8
Francisca <franciscabelen112002@gmail.com>
2020-06-11 10:38:45 -0400

    home con estilos

--- src/lib/view/templateHome.js
+++ src/lib/view/templateHome.js
@@ -3,18 +3,28 @@ export const home = () => {
   const viewHome = `   
   <div id="background">
   <div class=containerWeb Home>
-  <div id="homeContent"><h1 id="titleInicio" class="welcome">BIENVENIDO A <br> SOCIALBEER</h1>
+  <div id="homeContent">
+  <hr class="lineHome">
+  <h1 id="titleInicio" class="welcome">BIENVENIDO A <br> SOCIALBEER</h1>
   <h2 id="sloganInicio" class="slogan">Tú red social cervecera</h2>
+  <hr class="lineHome">
   <p>Te invitamos a participar en la comunibeer para interactuar con otros amantes de la cerveza</p>
   <button class=btnInicio href="#registrarse">¡Quiero ser parte!</button>
-  <div> <img id="logoInicio" src="./img/logo.png"> </div>
-  <div> <img id="logoMapa" src="./img/mapa.png"> </div>
+  </div>
+  <div class="imgLogoHome"> 
+  <img id="logoInicio" src="./img/logo.png"> 
+  </div>
   
-  </div></div></div>`
+  
+  </div>`
 
   divHome.innerHTML = viewHome;
   return divHome;
 }
+
+// </div>
+//   <div> <img id="logoMapa" src="./img/mapa.png">
+//   </div> 
 
 //  <a href="./">ver mayoría de edad</a>
 

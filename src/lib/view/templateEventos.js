31242dbcf81dcfb2bd17b18be741a23d81a32f8e
kthcsk <csk.kath@gmail.com>
2020-06-11 12:46:37 -0400

    galería eventos y centrar logo

--- src/lib/view/templateEventos.js
+++ src/lib/view/templateEventos.js
@@ -4,10 +4,39 @@ export const eventos = () => {
      <div id="banner">
         <img src="./img/bannerprueba1.png">
     </div>
-
-    <section id="canceled">
-         <img src="./img/eventoscancelados.png"> 
-    </section> 
+    <div class="carousel">
+      <div class="carousel-inner">
+        <input class="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden="" checked="checked">
+        <div class="carousel-item">
+          <img src="../src/img/prueba(borrar)/prueba1.png" alt="">
+        </div>
+        <input class="carousel-open" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden="">
+        <div class="carousel-item">
+          <img src="../src/img/prueba(borrar)/prueba2.png" alt="">
+        </div>
+        <input class="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" hidden="">
+        <div class="carousel-item">
+          <img src="../src/img/prueba(borrar)/prueba3.png" alt="">
+        </div>
+        <label for="carousel-3" class="carousel-control prev control-1">‹</label>
+        <label for="carousel-2" class="carousel-control next control-1">›</label>
+        <label for="carousel-1" class="carousel-control prev control-2">‹</label>
+        <label for="carousel-3" class="carousel-control next control-2">›</label>
+        <label for="carousel-2" class="carousel-control prev control-3">‹</label>
+        <label for="carousel-1" class="carousel-control next control-3">›</label>
+        <ol class="carousel-indicators">
+            <li>
+                <label for="carousel-1" class="carousel-bullet">•</label>
+            </li>
+            <li>
+                <label for="carousel-2" class="carousel-bullet">•</label>
+            </li>
+            <li>
+                <label for="carousel-3" class="carousel-bullet">•</label>
+            </li>
+        </ol>
+    </div>
+</div>
 `
 divEventos.innerHTML = viewEventos; 
 

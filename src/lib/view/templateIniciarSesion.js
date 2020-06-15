ed736b533b1dff90596fa463bd79ba5c8eec9751
María José Garrido Henriquez <mjgarrido.h@gmail.com>
2020-06-15 09:01:50 -0400

    cambios

--- src/lib/view/templateIniciarSesion.js
+++ src/lib/view/templateIniciarSesion.js
@@ -1,15 +1,3 @@
-/* import { login } from '../index.js'
-
-export const iniciarSesion = () => {
-  const divIniciarsesion = document.createElement('div');
-
-  const viewIniciarsesion = `<button id='google'>Ingresar con Google</button>`
-
-  divIniciarsesion.innerHTML = viewIniciarsesion;
-  const btn = divIniciarsesion.querySelector('#google');
-    btn.addEventListener('click', () => { login() })
-  return divIniciarsesion;
-}; */
 
 import {
   login

bf2c98c866c8885bd41faff501006199b82b99c5
Francisca <franciscabelen112002@gmail.com>
2020-06-15 11:57:06 -0400

    tomar actualizacion de iniciar sesion

--- src/lib/view/templateIniciarSesion.js
+++ src/lib/view/templateIniciarSesion.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 // import { registrarse } from './templateRegistrarse';
 import { login } from '../index.js';
 import { register } from './register.js';
@@ -16,16 +14,6 @@ export const iniciarSesion = () => {
     btn.addEventListener('click', () => { login() })
   return divIniciarsesion;
 }; */
-=======
-=======
-
-=======
->>>>>>> aedaa0e5a80e509a6da35765599eb1c172aa890c
->>>>>>> 45af2e54378aa23156bb18f37eb0cd55bce8dcd2
-import {
-  login
-} from '../index.js'
->>>>>>> aedaa0e5a80e509a6da35765599eb1c172aa890c
 
 export const iniciarSesion = () => {
   const divIniciarsesion = document.createElement('div');
@@ -47,18 +35,21 @@ export const iniciarSesion = () => {
 
   <div class="hello">
   <form class="formInicio" action="#">
-  <h1 class='titleInicio'>Crear una cuenta</h1>
+  <h1 class='titleInicio'>Registrarse</h1>
   <div class='social-container'>
   <button href='#' id='facebook' class='social'><i class='fab fa-facebook-f'></i></button>
   <button href='#' id='google' class='social'><i class='fab fa-google-plus-g'></i></button>
   </div>
   <span></span>
-  <input class="inputInicio" type="email" placeholder="Email" />
-  <input class="inputInicio" type="password" placeholder="Contraseña" />
-  <button class="btnInicio" id='btnRegistrarse'>Registrarse</button>
-</form> 
-  </div></div>`
+  <input id="emailRegistar" class="inputInicio" type="email" placeholder="Email" />
+  <input id="contrasenaRegistrar" class="inputInicio" type="password" placeholder="Contraseña" />
+  <button class="btnInicio" id='registrarUsuario'>Registrarse</button>
+  </form> 
 
+  </div>
+  
+  </div>
+`
 
   divIniciarsesion.innerHTML = viewIniciarsesion;
   const btn = divIniciarsesion.querySelector('#google');

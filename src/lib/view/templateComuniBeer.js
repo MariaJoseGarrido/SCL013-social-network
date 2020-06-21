5ddb2f83ce8b17ccdac774827c8cb934bd7fca36
Francisca <franciscabelen112002@gmail.com>
2020-06-21 18:55:31 -0400

    css linea cervecerias chilenas

--- src/lib/view/templateComuniBeer.js
+++ src/lib/view/templateComuniBeer.js
@@ -1,17 +1,17 @@
+import { iniciarSesion } from './templateIniciarSesion.js';
+import {currentUser, postLike} from '../firebase.js'
+
 export const comuniBeer = () => {
+  let user = currentUser();
   const divcomuniBeer = document.createElement("div");
   const viewcomuniBeer = ` 
   <div id="containerWebComunity">
-  <h1>Contenedor</h1>
   <header id="banner">
     <img class="logoBanner" src="http://imgfz.com/i/bGv4odX.png" alt="Logo SocialBeer">
   </header>
   <div id="containerSocialNetwork">
-    <h1>RedSocial</h1>
     <section id="reviewPostAndComments">
-      <h1>Contenedor Reseñas y Post</h1>
       <div id="review">
-        <h1>Reseñas</h1> 
         <h3>Crea tu reseña cervecera</h3>
           <div id="startAndImage">
             <div class="ec-stars-wrapper">
@@ -21,36 +21,33 @@ export const comuniBeer = () => {
               <a href="#" data-value="4" title="Votar con 4 estrellas">&#9733;</a>
               <a href="#" data-value="5" title="Votar con 5 estrellas">&#9733;</a>
             </div>
-            <div id="imageToUpload">
-            <input type="file" name="imageToUpload" id="btnImageUpload" class="inputTypeFile">
-            </div>
           </div>
         <br>
-        <input type="text" id="inputBeerBrand" placeholder='Marca de tu cerveza' class='inputWeb'></input>
-        <input type="text" id="inputBeerType" placeholder='Tipo de tu cerveza' class='inputWeb'></input>
-        <input type="text" id="inputCountry" placeholder='País' class='inputWeb'></input>
-        <textarea type='text' id="inputReviewBeer" placeholder='Reseña de tu cerveza' class='inputWeb review'></textarea>
+        <form>
+          <input type="text" autocomplete="off" id="inputBeerBrand" placeholder='Marca de tu cerveza' class='inputWeb'></input>
+          <input type="text" autocomplete="off" id="inputBeerName" placeholder='Nombre de tu cerveza (solo si tiene)' class='inputWeb'></input>
+          <input type="text" autocomplete="off" id="inputBeerType" placeholder='Tipo de tu cerveza' class='inputWeb'></input>
+          <input type="text" autocomplete="off" id="inputBeerCountry" placeholder='País' class='inputWeb'></input>
+          <textarea type='text' id="inputReviewBeer" placeholder='Reseña de tu cerveza' class='inputWeb review'></textarea>
+        </form>
         <br>
-        <button type="submit" class='btnWeb' id='btnReview'>¡Reseñar!</button>
+        <div id="containerBtnSubmit">
+          <label for="file-upload" class="btnImageUnpload">
+            <i class="fas fa-camera"></i>
+          </label>
+          <input id="file-upload" type="file" style='display: none;' accept="image/x.png, image/jpeg"/>
+          <div id="info"></div> 
+          <button type="submit" class='btnWeb' id='btnReview'>¡Reseñar!</button>  
+        </div>
+        
       </div>
       <div id="postAndComments">
-        <h1>Post Fijos y Comentarios</h1>
-        <div id="reviewPost"
-          <h1>Post Reseñas</h1>
+        <div id="reviewPost">
           <div id="rootReview"></div>
         </div>
-        <div id="comments">
-          <h1>Comentarios</h1>
-          <textarea type='text' id="inputCommentsBeer" placeholder='Escribe un comentario' class='inputWeb review'></textarea>
-          <br>
-          <button type="submit" class='btnWeb' id='btnComments'>¡comentar!</button>
-        </div>
-        <p>Fin Post Fijos y Comentarios</p>
       </div>
-      <p>fin contenedor reseñas y post</p>
     </section>
     <section id="profileAndOthers">
-      <h1>Contenedor Perfil y Otros</h1>
       <div id="profile">
         <h1>Perfil</h1>
         <div id="rootProfile">
@@ -61,97 +58,96 @@ export const comuniBeer = () => {
         <iframe src='https://open.spotify.com/embed/playlist/5vYkdZqio6UOLB1qtYGGVe?si=x3vZxUVlR3WUoaXy_tsg-A' width="300" height="380" frameborder='0' allowtransparency="true" allow="encrypted-media"></iframe>
       </div>
       </div>
-      <p>Fin Contenedor Perfil y Otros</p>
     </section>
-    <p>Fin contenedor RedSocial</p>
   </div>
-  <footer id="socialNetworkFooter">
-    <h1>pie de página</h1>
-  </footer>
-  <p>fin contenedor web</p>
   </div>
-
   <footer>
+  <div class="container-primFooter"> 
+  </div> 
+  <div class="container-finFooter">
+   
+    <div class="logoSocialBeer"> 
+      <a href="#home"> 
+      <img src="./img/logo.png" alt="Logo del sitio"> 
+    </div>
   
-      <div class="container-primFooter"> </div> 
-    
-      <div class="container-finFooter">
-        
-        <div class="logoSocialBeer"> 
-          <a href="#home"> <img src="./img/logo.png" alt="Logo del sitio"> </a>
-        </div>
-    
-        <div class="menuFooter">
-          <p class="opmenuFooter">
-            <a href="#blog">Blog </a>
-            | &nbsp;
-            <a href="#comunibeer">Comunibeer </a>
-            | &nbsp;
-            <a href="#dondebeber">¿Dónde beber? </a>
-            | &nbsp;
-            <a href="#eventos">Eventos </a>
-            | &nbsp;
-            <a href="#abastecete">¡Abastécete! </a>
-            | &nbsp;
-            <a href="#cerveceriasChilenas">Cervecerías Chilenas </a>
-          </p>
-    
-          <p class="menuiconsFooter"> 
-            <a href="#home"> 
-              <i class="fa fa-home iconFooter"> </i> 
-            </a>
-            <a href="#iniciarsesion"> 
-              <i class="fa fa-user iconFooter"></i> 
-            </a> 
-            <a href="#contacto"> 
-              <i class="fas fa-phone iconFooter"></i> 
-            </a>
-            <a href="https://open.spotify.com/embed/playlist/5vYkdZqio6UOLB1qtYGGVe?si=x3vZxUVlR3WUoaXy_tsg-A" target="_blank"> 
-              <i class="fas fa-music iconFooter"></i> 
-            </a>
-          </p>
-    
-            <p class="copyrigth">
-              © 2020 Social Beer™  | Todos los derechos reservados.
-            </p>
-          </div>
-        </div>
-    
-      </footer>
+    <div class="menuFooter">
+      <p class="opmenuFooter">
+        <a href="#blog"> Blog </a>
+        | &nbsp;
+        <a href="#comunibeer"> Comunibeer </a>
+        | &nbsp;
+        <a href="#dondebeber"> ¿Dónde beber? </a>
+        | &nbsp;
+        <a href="#eventos"> Eventos </a>
+        | &nbsp;
+        <a href="#abastecete"> ¡Abastécete! </a>
+        | &nbsp;
+        <a href="#cerveceriasChilenas"> Cervecerías Chilenas </a>
+      </p>
+  
+      <p class="menuiconsFooter"> 
+        <a href="#home"> 
+          <i class="fa fa-home iconFooter"> </i> 
+        </a>
+        <a href="#iniciarsesion"> 
+          <i class="fa fa-user iconFooter"></i> 
+        </a> 
+        <a href="#contacto"> 
+          <i class="fas fa-phone iconFooter"></i> 
+        </a>
+        <a href="https://open.spotify.com/embed/playlist/5vYkdZqio6UOLB1qtYGGVe?si=x3vZxUVlR3WUoaXy_tsg-A" target="_blank"> 
+          <i class="fas fa-music iconFooter"></i> 
+        </a>
+      </p>
+  
+        <p class="copyrigth">
+          ©️ 2020 Social Beer™️  | Todos los derechos reservados.
+        </p>
+      </div>
+      
+    </div>
+  
+  </footer>
   `
   divcomuniBeer.innerHTML = viewcomuniBeer;
   const btnReviewBeer = divcomuniBeer.querySelector('#btnReview');
   btnReviewBeer.addEventListener('click', guardar);
-  
-
 
   //Inicia firestore
   var db = firebase.firestore();
 
-
-  //Agregar comentarios
+  //Agregar comentarios 
 
   function guardar(){
     let beerBrand = document.getElementById('inputBeerBrand').value;
+    let beerName = document.getElementById('inputBeerName').value;
     let beerType = document.getElementById('inputBeerType').value;
-    let beerCountry = document.getElementById('inputCountry').value;
+    let beerCountry = document.getElementById('inputBeerCountry').value;
     let beerReview = document.getElementById('inputReviewBeer').value;
-    let beerImg = document.getElementById('btnImageUpload').value;
+    let beerImg = document.getElementById('file-upload').value;
 
   db.collection("resenas").add({
+      fechaPublicacion: Date.now(),
       marcaCerveza: beerBrand,
+      nombreCerveza: beerName,
       tipoCerveza: beerType,
       paisCerveza: beerCountry,
       resenaCerveza: beerReview,
-      imagenCerveza: beerImg
+      imagenCerveza: beerImg,
+      userId: user.uid,
+      likes: ""
   })
+
+
   .then(function(docRef) {
       console.log("Document written with ID: ", docRef.id);
       document.getElementById('inputBeerBrand').value='';
+      document.getElementById('inputBeerName').value='';
       document.getElementById('inputBeerType').value='';
-      document.getElementById('inputCountry').value='';
+      document.getElementById('inputBeerCountry').value='';
       document.getElementById('inputReviewBeer').value='';
-      document.getElementById('btnImageUpload').value='';
+      document.getElementById('file-upload').value='';
   })
   .catch(function(error) {
       console.error("Error adding document: ", error);
@@ -160,29 +156,135 @@ export const comuniBeer = () => {
 
 //leer doc
 
-db.collection("resenas").onSnapshot((querySnapshot) => {
+db.collection("resenas").orderBy("fechaPublicacion", "desc").onSnapshot((querySnapshot) => {
     document.getElementById("rootReview").innerHTML='';
-    querySnapshot.forEach((doc) => { //forEach ciclos que se repiten en el documento para imprimir el dato
-    console.log(`${doc.id} => ${doc.data().marcaCerveza}`);
-    document.getElementById("rootReview").innerHTML += `
-    <section id="textReview">
-    <p>${doc.id}</p>
-    <img ${doc.data().imagenCerveza}>
-    <h4>${doc.data().marcaCerveza}</h4>
-    <h5>${doc.data().tipoCerveza}</h5>
-    <h5>${doc.data().paísCerveza}</h5>
-    <p>${doc.data().resenaCerveza}</p>
-    </section>
+    querySnapshot.forEach((doc) => {
+      let divPost = document.createElement("div"); //forEach ciclos que se repiten en el documento para imprimir el dato   
+      divPost.innerHTML = `
+    <section id="containerReviewPost">
+      <div id="post">
+        <div id="menuBtn">
+          <a class="btnEditDelete"><i class="fas fa-trash-alt" id="delete-${doc.id}"></i>
+          </a>
+          <a class="btnEditDelete"><i class="fas fa-edit" id="edit-${doc.id}"></i>
+          </a>
+        </div>
+        <div id="textReviewPost">
+          <div id="labelColumn">
+            <p class="labelText"> MARCA: </p>
+            <p class="labelText"> Nombre: </p>
+            <p class="labelText"> TIPO: </p>
+            <p class="labelText"> PAÍS: </p>
+          </div>
+          <div id="inputEntranceColumn">
+            <p class="reviewLabel" id="marca"> <strong> ${doc.data().marcaCerveza} </strong> </p>
+            <p class="reviewLabel" id="nombre"> <strong> ${doc.data().nombreCerveza} </strong> </p>
+            <p class="reviewLabel" id="tipo"> <strong> ${doc.data().tipoCerveza}</strong> </p>
+            <p class="reviewLabel" id="pais"> <strong> ${doc.data().paisCerveza}</strong> </p>
+          </div>
+        </div>
+          <div id="reviewRow">
+          <p id="res">${doc.data().resenaCerveza}</p>    
+          <button class="botonGuardar" id="guardar-${doc.id}">Guardar</button>      
+        </div>
+          <img ${doc.data().imagenCerveza}></img>
+        <div id="likeBtn">
+          <a class="btnImageUnpload" id="like-${doc.id}"><i class="fas fa-beer"> ${doc.data().likes.length}</i>   
+          </a>          
+        </div>
+      </div>
+        <div id="comments">
+          <textarea type='text' id="inputCommentsBeer" placeholder='Escribe un comentario' class='inputWeb review'></textarea>
+          <br>
+          <button type="submit" class='btnWeb' id='btnComments'>¡comentar!</button>
+        </div>  
+    </section>    
     `
+    const del = divPost.querySelector(`#delete-${doc.id}`)
+      del.addEventListener('click', () => {
+        console.log("entro al boton")
+        db.collection("resenas").doc(doc.id).delete()
+        .then( () => {
+        console.log("Document successfully deleted!");
+        })
+        .catch( (error) => {
+        console.error("Error removing document: ", error);
+        });
+    })
+
+    
+      const boton = divPost.querySelector(`#guardar-${doc.id}`); 
+      boton.style.display = "none"; 
+      const edit =  divPost.querySelector(`#edit-${doc.id}`)
+      edit.addEventListener('click', () => {
+       // console.log(user.id)
+        console.log(doc.userId)  
+        console.log(divPost.querySelector("#inputEntranceColumn").textContent)      
+       // if (user.id === doc.userId){        
+        let res = divPost.querySelector("#res");
+        let nombre = divPost.querySelector("#nombre");
+        let pais = divPost.querySelector("#pais");
+        let marca = divPost.querySelector("#marca");
+        let tipo = divPost.querySelector("#tipo");
+
+          nombre.setAttribute("contenteditable", true);
+          tipo.setAttribute("contenteditable", true);
+          pais.setAttribute("contenteditable", true);
+          res.setAttribute("contenteditable", true);
+          marca.setAttribute("contenteditable", true);
+         
+          boton.style.display = "block";
+          boton.addEventListener('click', () => {
+            console.log("Entró al boton guardar")
+            db.collection("resenas").doc(doc.id).update({
+              marcaCerveza: marca.textContent,
+              nombreCerveza: nombre.textContent,
+              tipoCerveza: tipo.textContent,
+              paisCerveza: pais.textContent,
+              resenaCerveza: res.textContent,
+            })
+            .then( () => {
+            console.log("Document successfully updated!");
+            nombre.setAttribute("contenteditable", false);
+          tipo.setAttribute("contenteditable", false);
+          pais.setAttribute("contenteditable", false);
+          res.setAttribute("contenteditable", false);
+          marca.setAttribute("contenteditable", false);
+           boton.style.display = "none"; 
+            })
+            .catch( (error) => {
+            console.error("Error updating document: ", error);
+            });
+          })                
+       /* }else{
+          alert("No puedes editar un post que no creaste")
+        }*/
+    })
+
+
+    //LIKESSSS
+    const like = divPost.querySelector(`#like-${doc.id}`)
+        like.addEventListener('click', () => {
+        postLike(doc.id);
+      })
+
+    document.getElementById("rootReview").appendChild(divPost).innerHTML
   });
 })
 
 
+//SUBIR IMAGENES 
+
+/* document.getElementById("file-upload").change(() => {
+  if(this.file && this.files [0]){
+    let Archivo= new FileReader();
+    Archivo.onload= () => 
+  } 
+
+});*/
 
 
 
 return divcomuniBeer;
 
 };
-
-
